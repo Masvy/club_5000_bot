bc627be3c4ba70dd378e28df0f168412ade328f8
Masvy <masvy.435@gmail.com>
2023-07-06 15:39:28 +0300

    refactor: register two routers

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from config_data.config import ConfigBot, load_config
+from config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers
 
 # Инициализировал логгер
 logger = logging.getLogger(__name__)
@@ -15,6 +16,7 @@ async def main():
     Конфигурировал логирование
     Вывел в консоль информацию о начале запуска бота
     Загруpbk конфиг в переменную config
+    Регистриуем роутеры в диспетчере
     Пропускаем накопившиеся апдейты и запускаем polling
     '''
     logging.basicConfig(
@@ -24,10 +26,13 @@ async def main():
 
     logger.info('Starting bot')
 
-    config: ConfigBot = load_config()
+    config: Config = load_config()
     bot: Bot = Bot(token=config.tg_bot.token,
                    parse_mode='HTML')
     dp: Dispatcher = Dispatcher()
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
