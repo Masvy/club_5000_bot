f9f35a84dfd0a8569da774e9633bf7ed3358d2fc
Masvy <masvy.435@gmail.com>
2023-07-06 19:26:06 +0300

    feat: add a sequence of questions for verification

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,13 +1,45 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, StateFilter
 from aiogram.types import Message
+from aiogram.fsm.state import default_state
+from aiogram.fsm.context import FSMContext
 from lexicon.user_lexicon import USER_LEXICON
+from states.user_states import Verification
 
 # Инициализировал роутер для данного модуля
 router: Router = Router()
 
 
 # Этот хэндлер отвечает на команду /start
-@router.message(CommandStart())
-async def start_user_bot(message: Message):
+# И переводить бота в состояние ожидания ввода номера
+@router.message(CommandStart(), StateFilter(default_state))
+async def start_user_bot(message: Message, state: FSMContext):
     await message.answer(text=USER_LEXICON['/start'])
+    await state.set_state(Verification.number)
+
+
+# Этот эндлер отлавливает номер
+# И переводит бота в состояние ожидания ввода имени
+@router.message(StateFilter(Verification.number))
+async def process_number_sent(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    await message.answer(text=USER_LEXICON['input_name'])
+    await state.set_state(Verification.name)
+
+
+# Этот хэндлер отлавливает имя
+# И переводит бота в состояние ожидания ввода города
+@router.message(StateFilter(Verification.name))
+async def process_name_sent(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer(text=USER_LEXICON['input_city'])
+    await state.set_state(Verification.city)
+
+
+# Это хендлер отлавливает город
+# И выключает FSM
+@router.message(StateFilter(Verification.city))
+async def process_city_sent(message: Message, state: FSMContext):
+    await state.update_data(city=message.text)
+    await message.answer(text=USER_LEXICON['FSM_finish'])
+    await state.clear()
