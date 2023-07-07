8b2be9b93e72705b625a05687c0b28914004bceb
Masvy <masvy.435@gmail.com>
2023-07-07 13:17:52 +0300

    chore: add a new button obj and a new keyboard obj

--- keyboards/reply_user.py
+++ keyboards/reply_user.py
@@ -7,9 +7,15 @@ menu_but2: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['status_but'])
 menu_but3: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['join_but'])
 menu_but4: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['dues_but'])
 
+phone_num_but: KeyboardButton = KeyboardButton(text='Отправить номер', request_contact=True)
+
 # Создал объект клавиатуры, добавил кнопки
 menu_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
     keyboard=[[menu_bit1, menu_but2],
               [menu_but3, menu_but4]],
     resize_keyboard=True,
     one_time_keyboard=True)
+
+phone_num_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+    keyboard=[[phone_num_but]],
+    resize_keyboard=True)
