49c3300236843ba8c7db989554d9d7b268219ecb
Masvy <masvy.435@gmail.com>
2023-07-07 13:25:17 +0300

    fix: delete extra button object

--- keyboards/reply_user.py
+++ keyboards/reply_user.py
@@ -3,16 +3,15 @@ from lexicon.user_lexicon import MENU_USER_LEXICON
 
 # Создал объекты кнопок
 menu_bit1: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['rules_but'])
-menu_but2: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['status_but'])
-menu_but3: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['join_but'])
-menu_but4: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['dues_but'])
+menu_but2: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['join_but'])
+menu_but3: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['dues_but'])
 
 phone_num_but: KeyboardButton = KeyboardButton(text='Отправить номер', request_contact=True)
 
 # Создал объект клавиатуры, добавил кнопки
 menu_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
     keyboard=[[menu_bit1, menu_but2],
-              [menu_but3, menu_but4]],
+              [menu_but3]],
     resize_keyboard=True,
     one_time_keyboard=True)
 
