a5b2a48cfd4c0fd91facccaea1b0afd5c067fb0e
Masvy <masvy.435@gmail.com>
2023-07-07 12:20:47 +0300

    feat: create a keyboard (not inline )
    create folder keyboards(__init__.py, reply_user.py)
    
    create button objects and a keyboard object in reply_user.py

---
+++ keyboards/reply_user.py
@@ -0,0 +1,15 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from lexicon.user_lexicon import MENU_USER_LEXICON
+
+# Создал объекты кнопок
+menu_bit1: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['rules_but'])
+menu_but2: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['status_but'])
+menu_but3: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['join_but'])
+menu_but4: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['dues_but'])
+
+# Создал объект клавиатуры, добавил кнопки
+menu_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+    keyboard=[[menu_bit1, menu_but2],
+              [menu_but3, menu_but4]],
+    resize_keyboard=True,
+    one_time_keyboard=True)
