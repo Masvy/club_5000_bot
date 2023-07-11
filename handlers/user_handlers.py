145017fefffaaaa75db5a978da36654706957ae1
Masvy <masvy.435@gmail.com>
2023-07-11 12:40:16 +0300

    feat: called functions to write and read db data

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 from lexicon.user_lexicon import USER_LEXICON
 from states.user_states import Verification
 from keyboards.reply_user import menu_kb, phone_num_kb
+from db.users import add_user_id, add_name, add_city, read_name
 
 # Инициализировал роутер для данного модуля
 router: Router = Router()
@@ -15,6 +16,8 @@ router: Router = Router()
 # И переводить бота в состояние ожидания получения номера
 @router.message(CommandStart(), StateFilter(default_state))
 async def start_user_bot(message: Message, state: FSMContext):
+    _user_id = message.from_user.id
+    await add_user_id(_user_id)
     await message.answer(text=USER_LEXICON['/start'],
                          reply_markup=phone_num_kb)
     await state.set_state(Verification.number)
@@ -34,6 +37,9 @@ async def process_number_sent(message: Message, state: FSMContext):
 # И переводит бота в состояние ожидания ввода города
 @router.message(StateFilter(Verification.name))
 async def process_name_sent(message: Message, state: FSMContext):
+    _user_id = message.from_user.id
+    name = message.text
+    await add_name(_user_id, name)
     await state.update_data(name=message.text)
     await message.answer(text=USER_LEXICON['input_city'])
     await state.set_state(Verification.city)
@@ -44,9 +50,15 @@ async def process_name_sent(message: Message, state: FSMContext):
 @router.message(StateFilter(Verification.city))
 async def process_city_sent(message: Message, state: FSMContext):
     photo = FSInputFile('photo/badge_5000_social.jpg')
+    _user_id = message.from_user.id
+    city_name = message.text
+    name_user = await read_name(_user_id)
     await state.update_data(city=message.text)
+    await add_city(_user_id, city_name)
     await message.answer(text=USER_LEXICON['FSM_finish'])
-    await message.answer_photo(photo=photo, caption=USER_LEXICON['greetings'],
+    await message.answer_photo(photo=photo,
+                               caption=f"Привет, {name_user[0]}"
+                               f"\n\n{USER_LEXICON['greetings']}",
                                reply_markup=menu_kb)
     await state.clear()
 
@@ -55,8 +67,12 @@ async def process_city_sent(message: Message, state: FSMContext):
 @router.message(Text(text='Правила'))
 async def show_rules(message: Message):
     photo = FSInputFile('photo/badge_5000_social.jpg')
+    _user_id = message.from_user.id
+    name_user = await read_name(_user_id)
     await message.answer(text=USER_LEXICON['rules'])
-    await message.answer_photo(photo=photo, caption=USER_LEXICON['greetings'])
+    await message.answer_photo(photo=photo,
+                               caption=f"Привет, {name_user[0]}"
+                               f"\n\n{USER_LEXICON['greetings']}")
 
 
 # Этот хэндлер отвечает на нажатие кнопки 'Вступить'
