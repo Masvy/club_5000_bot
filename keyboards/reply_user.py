d07aacb4677f8acb6c18f54b6cb69cd90ded9506
Masvy <masvy.435@gmail.com>
2023-07-16 14:29:34 +0300

    feat: create new button object

--- keyboards/reply_user.py
+++ keyboards/reply_user.py
@@ -5,13 +5,14 @@ from lexicon.user_lexicon import MENU_USER_LEXICON
 menu_bit1: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['rules_but'])
 menu_but2: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['join_but'])
 menu_but3: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['dues_but'])
+menu_but4: KeyboardButton = KeyboardButton(text=MENU_USER_LEXICON['achievements'])
 
 phone_num_but: KeyboardButton = KeyboardButton(text='Отправить номер', request_contact=True)
 
 # Создал объект клавиатуры, добавил кнопки
 menu_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
     keyboard=[[menu_bit1, menu_but2],
-              [menu_but3]],
+              [menu_but3, menu_but4]],
     resize_keyboard=True,
     one_time_keyboard=True)
 
