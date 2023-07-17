3f6b68f77512af7e48bc5cee72952100b4b83d91
Masvy <masvy.435@gmail.com>
2023-07-17 14:06:00 +0300

    feat: register a new router

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage, Redis
 from config_data.config import Config, load_config
-from handlers import new_user_handlers, other_handlers, member_handlers
+from handlers import new_user_handlers, other_handlers
+from handlers import member_handlers, admin_handlers
 from db import BaseModel, proceed_schemas
 from loader import session_maker, async_engine
 
@@ -44,6 +45,7 @@ async def main():
 
     dp.include_router(new_user_handlers.router)
     dp.include_router(member_handlers.router)
+    dp.include_router(admin_handlers.router)
     dp.include_router(other_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
