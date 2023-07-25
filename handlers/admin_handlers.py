1832aaf7cd572d0e89b3ec14dd55684519b91525
Masvy <masvy.435@gmail.com>
2023-07-25 13:11:35 +0300

    feat: create new handlers

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,32 +1,62 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import CallbackQuery
-from aiogram.filters import Text
 
 from filters.member_filters import IsStatus
-from db.users import read_status_member, read_name
+from db.users import read_name
 from lexicon.user_lexicon import USER_LEXICON
-from keyboards.reply_admin import admin_menu_kb
+from lexicon.admin_lexicon import MENU_ADMIN_PANEL
+from keyboards.inline_admin import admin_menu_kb, admin_panel_menu_kb, user_manage_kb
 
 # Инициализировал роутер для данного модуля
 router: Router = Router()
 
-file_id = [
-    'AgACAgIAAxkDAAIElGSv9ME7laADL8wTKuQFqrSZS'
-    'hhHAALSyTEbTmBgSWiUvIVKpFmsAQADAgADeAADLwQ'
-]
 
 status_member_list: list[str] = ['Админ']
 
-router.message.filter(IsStatus(status_member_list))
 
-
-@router.callback_query(Text(text='okay_but_pressed'))
+@router.callback_query(F.data == 'okay_but_pressed',
+                       IsStatus(status_member_list))
 async def show_menu_3(callback: CallbackQuery):
-    status = await read_status_member(callback.from_user.id)
     name = await read_name(callback.from_user.id)
-    if status == 'Админ':
-        await callback.message.answer_photo(photo=file_id[0],
-                                            caption=f'Привет, {name}👋\n\n'
-                                            'Сейчас твой уровень: Админ\n\n'
-                                            f'{USER_LEXICON["greetings"]}',
-                                            reply_markup=admin_menu_kb)
+    await callback.message.edit_text(text=f'Привет, {name}👋\n\n'
+                                     'Сейчас твой уровень: Админ\n\n'
+                                     f'{USER_LEXICON["greetings"]}',
+                                     reply_markup=admin_menu_kb)
+
+
+@router.callback_query(F.data == 'admin_panel_menu_pressed')
+async def show_admin_panel(callback: CallbackQuery):
+    await callback.message.edit_text(text=MENU_ADMIN_PANEL['show_admin_panel'],
+                                     reply_markup=admin_panel_menu_kb)
+
+
+@router.callback_query(F.data == 'statistics_but_pressed')
+async def show_stat(callback: CallbackQuery):
+    await callback.message.edit_text(text='Статистика проекта:\n\n'
+                                     'Сборов: \n\n'
+                                     'Пользователей (прошедших регистрацию):'
+                                     '\n\nВне клуба:\nЛайт:\nСтандарт:\n'
+                                     'Макс:\nАмбассадоров:\n\nАктивных:\n'
+                                     'за неделю:\nза месяц:\n\nСобрано'
+                                     ' средств:\nза неделю\nза месяц:',
+                                     reply_markup=admin_panel_menu_kb)
+
+
+@router.callback_query(F.data == 'users_manage_but_pressed')
+async def show_users_manage(callback: CallbackQuery):
+    await callback.message.edit_text(text=MENU_ADMIN_PANEL['users_manage_panel'],
+                                     reply_markup=user_manage_kb)
+
+
+@router.callback_query(F.data == 'block_user_but_pressed')
+async def func_block_user(callback: CallbackQuery):
+    await callback.message.edit_text(text=MENU_ADMIN_PANEL['input_user_id'])
+
+
+@router.callback_query(F.data == 'come_back_but_pressed')
+async def back_to_main_menu(callback: CallbackQuery):
+    name = await read_name(callback.from_user.id)
+    await callback.message.edit_text(text=f'Привет, {name}👋\n\n'
+                                     'Сейчас твой уровень: Админ\n\n'
+                                     f'{USER_LEXICON["greetings"]}',
+                                     reply_markup=admin_menu_kb)
